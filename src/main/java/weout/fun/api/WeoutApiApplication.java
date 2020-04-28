039bf81f8e5332ac0134d04c6bd86bc1992ec022
yannick-kouakam <ykouakam@gmail.com>
2020-04-28 21:23:47 +0200

    first commit

---
+++ src/main/java/weout/fun/api/WeoutApiApplication.java
@@ -0,0 +1,13 @@
+package weout.fun.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeoutApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeoutApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/weout/fun/api/WeoutApiApplicationTests.java
@@ -0,0 +1,13 @@
+package weout.fun.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeoutApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
